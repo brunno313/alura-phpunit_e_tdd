fdc184676c68bd01e810f10eba6f04f5927584d1
Bruno Santana <brunoalexandre313@gmail.com>
2020-03-02 11:35:53 -0300

    Refactor method avalia() for pass in test

--- leilao/src/Service/Avaliador.php
+++ leilao/src/Service/Avaliador.php
@@ -9,12 +9,14 @@ class Avaliador {
   /**
    * @var float
    */
-  private $maiorValor;
+  private $maiorValor = -INF;
 
   public function avalia(Leilao $leilao): void {
-      $lances = $leilao->getLances();
-      $ultimoLance = $lances[count($lances) - 1];
-      $this->maiorValor = $ultimoLance->getValor();
+      foreach ($leilao->getLances() as $lance) {
+        if ($lance->getValor() > $this->maiorValor) {
+          $this->maiorValor = $lance->getValor();
+        }
+      }
   }
 
   public function getMaiorValor(): float {
