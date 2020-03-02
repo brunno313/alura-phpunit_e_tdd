2ed54e30580198a5089bd8d6655ebe9c1754e421
Bruno Santana <brunoalexandre313@gmail.com>
2020-03-02 11:23:23 -0300

    Altera testes para pasta correta

---
+++ leilao/tests/Service/AvaliadorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Alura\Leilao\Tests\Service;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+use PHPUnit\Framework\TestCase;
+
+class AvaliadorTest extends TestCase {
+
+  /**
+   * Test um.
+   */
+  public function testUm() {
+    // Arrange - Given
+    $leilao = new Leilao('Fiat 147 0KM');
+
+    $maria = new Usuario('Maria');
+    $joao = new Usuario('João');
+
+    $leilao->recebeLance(new Lance($joao, 2000));
+    $leilao->recebeLance(new Lance($maria, 2500));
+
+    $leiloeiro = new Avaliador();
+
+    // Act - When
+    $leiloeiro->avalia($leilao);
+
+    $maiorValor = $leiloeiro->getMaiorValor();
+
+    // Assert - Then
+    $this->assertEquals(2500, $maiorValor);
+  }
+
+}
